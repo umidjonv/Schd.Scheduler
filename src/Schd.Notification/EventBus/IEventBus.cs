138ff3073e79575e6854f0554fdd0dcbb997308a
umidjonv <vumid85@mail.ru>
2021-08-27 18:22:07 +0500

    structure ...

---
+++ src/Schd.Notification/EventBus/IEventBus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace Schd.Notification.EventBus
+{
+    public interface IEventBus<T> where T: class
+    {
+        public IConsumer<T> Consumer { get; set; }
+        
+    }
+}
