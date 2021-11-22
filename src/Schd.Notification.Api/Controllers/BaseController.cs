3d2947e68ad590dbed8edb4dbd4802b8a09b2858
umidjonv <vumid85@mail.ru>
2021-11-22 16:29:58 +0500

    add migration project

--- src/Schd.Notification.Api/Controllers/BaseController.cs
+++ src/Schd.Notification.Api/Controllers/BaseController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Schd.Common.Response;
 
 namespace Schd.Notification.Controllers
